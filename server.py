7d5e2c50129de317689bc5db967e62abcb18cc03
darrylj1999 <darrylj1999@darrylj1999s-MacBook-Air.local>
2023-11-27 02:38:40 -0500

    feature extraction

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import pickle
+import pandas as pd
 from flask import Flask, request
 from phishing_detection_url.URLFeatureExtraction import featureExtraction
 
@@ -7,10 +8,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_phishing_score():
-    package = ""
+    features = []
     for _, url in request.args.items():
-        print( featureExtraction(url) )
-    return "<p> Hello World </p>"
+        features.append( featureExtraction(url) )
+    feature_names = ['Domain', 'Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection', 
+                        'https_Domain', 'TinyURL', 'Prefix/Suffix', 'DNS_Record', 'Web_Traffic', 
+                        'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over','Right_Click', 'Web_Forwards', 'Label']
+    features = pd.DataFrame(features, columns=feature_names)
+    features = features.drop( ['Domain'], axis=1 )
+    return features
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
