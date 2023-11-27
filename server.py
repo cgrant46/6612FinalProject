f93e7634ff93ab43a45150a14fb95fac805ee8f3
darrylj1999 <darrylj1999@darrylj1999s-MacBook-Air.local>
2023-11-27 02:23:43 -0500

    server args

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import pickle
 from flask import Flask, request
+from phishing_detection_url.URLFeatureExtraction import featureExtraction
 
 clf = pickle.load(open("phishing_detection_url/XGBoostClassifier.pickle.dat", "rb"))
 app = Flask(__name__)
 
 @app.route("/")
 def get_phishing_score():
-    return request.args
+    package = ""
+    for url in request.args.items():
+        package += url + '\n'
+    return package
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
