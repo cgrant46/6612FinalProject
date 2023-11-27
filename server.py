d1c304dae8d50f53840ac5cef69c02f0a280f012
darrylj1999 <darrylj1999@darrylj1999s-MacBook-Air.local>
2023-11-27 02:43:26 -0500

    feature extraction

--- server.py
+++ server.py
@@ -15,7 +15,6 @@ def get_phishing_score():
                         'https_Domain', 'TinyURL', 'Prefix/Suffix', 'DNS_Record', 'Web_Traffic', 
                         'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over','Right_Click', 'Web_Forwards']
     features = pd.DataFrame(features, columns=feature_names)
-    features = features.drop( ['Domain'], axis=1 )
     return features
 
 if __name__ == '__main__':
