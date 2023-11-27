aa1fa0bb3f1bfbb976ab3719fa9c90acabb6e1d1
darrylj1999 <darrylj1999@darrylj1999s-MacBook-Air.local>
2023-11-27 02:42:58 -0500

    feature extraction

--- server.py
+++ server.py
@@ -11,9 +11,9 @@ def get_phishing_score():
     features = []
     for _, url in request.args.items():
         features.append( featureExtraction(url) )
-    feature_names = ['Domain', 'Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection', 
+    feature_names = ['Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection', 
                         'https_Domain', 'TinyURL', 'Prefix/Suffix', 'DNS_Record', 'Web_Traffic', 
-                        'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over','Right_Click', 'Web_Forwards', 'Label']
+                        'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over','Right_Click', 'Web_Forwards']
     features = pd.DataFrame(features, columns=feature_names)
     features = features.drop( ['Domain'], axis=1 )
     return features
